a5154e479e18df1fedce489483a100317eeb0144
Paulo Sorrentino <paulo.sorrentino@luizalabs.com>
2017-08-04 13:10:00 -0300

    Merge json

--- Mix/Merge-Objects/scripts.js
+++ Mix/Merge-Objects/scripts.js
@@ -216,7 +216,226 @@ const newOrderJSON = {
   'emailTemplateRef': ''
 };
 
-const finalOrderJSON = deepmerge(pickUpOrderJSON, newOrderJSON);
+const invoiceJSON = {
+  'id': 32736329,
+  'customer': {
+    'id': 17319942,
+    'name': 'Cristiano Pereira Abade',
+    'nickname': 'Cristiano Pereira',
+    'mail': 'hostinhomello10@gmail.com',
+    'cpfCnpj': '04116114537',
+    'uuid': '4cc60f30-268d-461d-a972-afdeeb2f397e'
+  },
+  'payment': {
+    'id': 10,
+    'description': 'Cartão de Crédito Visa'
+  },
+  'orderCustomer': '5315500256268252',
+  'createdAt': '2017-04-26 09:06:54',
+  'freight': '27.30',
+  'amount': '495.00',
+  'term': 5,
+  'expectedDate': '04/05/2017',
+  'expectedDateWeek': 'Quinta-feira, 04 de Maio',
+  'products': [
+    {
+      'id': 38868722,
+      'orderId': 32736329,
+      'product': '0230011',
+      'model': '00',
+      'description': 'Kit 3 em 1 Britânia com Liquidificador Batedeira',
+      'brand': 'britânia',
+      'amount': '199.90',
+      'quantity': '02',
+      'term': 5,
+      'store': 204,
+      'idSeller': 'magazineluiza',
+      'nameSeller': 'Magazine Luiza',
+      'totalItem': '399.80',
+      'expectedDate': 'Sexta-feira, 11 de Agosto',
+      'expectedDateMessage': 'Entrega prevista para Sexta-feira, 11 de Agosto'
+    },
+    {
+      'id': 38868721,
+      'orderId': 32736329,
+      'product': '0887491',
+      'model': '00',
+      'description': 'Sanduicheira/Mini Grill Cadence Easy Meal Color',
+      'brand': 'cadence',
+      'amount': '59.90',
+      'quantity': '01',
+      'term': 5,
+      'store': 204,
+      'idSeller': 'magazineluiza',
+      'nameSeller': 'Magazine Luiza',
+      'totalItem': '59.90',
+      'expectedDate': 'Sexta-feira, 11 de Agosto',
+      'expectedDateMessage': 'Entrega prevista para Sexta-feira, 11 de Agosto'
+    },
+    {
+      'id': 38868720,
+      'orderId': 32736329,
+      'product': '493074',
+      'model': '00',
+      'description': 'Troca Certa',
+      'brand': 'LUISASEG',
+      'amount': '8.00',
+      'quantity': '01',
+      'idSeller': 'magazineluiza',
+      'nameSeller': 'Magazine Luiza',
+      'totalItem': '8.00',
+      'expectedDate': 'Sexta-feira, 04 de Agosto',
+      'financialServiceDescription': 'TC',
+      'expectedDateMessage': 'Entrega prevista para Sexta-feira, 04 de Agosto'
+    }
+  ],
+  'contracts': [
+    {
+      'orderId': 32736329,
+      'productId': 23001100,
+      'slipId': 176495,
+      'erpOrderId': 369492504,
+      'invoiceAddress': {
+        'branchId': 630,
+        'invoiceId': 509172,
+        'invoiceDate': '27/04/2017',
+        'customerName': 'CRISTIANO PEREIRA ABADE',
+        'invoiceType': ' ',
+        'erpId': 369492504,
+        'address': 'CARLOS PIVA, 493 CASA',
+        'neighborhood': 'JD TRES MARIAS',
+        'zipcode': 8331,
+        'zipcodeComplement': 180,
+        'zipcodeComplete': '8331-180',
+        'city': 'SAO PAULO',
+        'state': 'SP',
+        'phoneAreaCode': 11,
+        'phoneNumber': 20158406,
+        'cgcd': '4116114537'
+      },
+      'invoices': [
+        {
+          'branchId': 630,
+          'invoiceId': 509172,
+          'invoiceDate': '27/04/2017',
+          'customerName': 'CRISTIANO PEREIRA ABADE',
+          'invoiceType': ' ',
+          'erpId': 369492504,
+          'address': 'CARLOS PIVA, 493 CASA',
+          'neighborhood': 'JD TRES MARIAS',
+          'zipcode': 8331,
+          'zipcodeComplement': 180,
+          'zipcodeComplete': '8331-180',
+          'city': 'SAO PAULO',
+          'state': 'SP',
+          'phoneAreaCode': 11,
+          'phoneNumber': 20158406,
+          'cgcd': '4116114537'
+        }
+      ]
+    },
+    {
+      'orderId': 32736329,
+      'productId': 88749100,
+      'slipId': 176495,
+      'erpOrderId': 369492504,
+      'invoiceAddress': {
+        'branchId': 630,
+        'invoiceId': 509172,
+        'invoiceDate': '27/04/2017',
+        'customerName': 'CRISTIANO PEREIRA ABADE',
+        'invoiceType': ' ',
+        'erpId': 369492504,
+        'address': 'CARLOS PIVA, 493 CASA',
+        'neighborhood': 'JD TRES MARIAS',
+        'zipcode': 8331,
+        'zipcodeComplement': 180,
+        'zipcodeComplete': '8331-180',
+        'city': 'SAO PAULO',
+        'state': 'SP',
+        'phoneAreaCode': 11,
+        'phoneNumber': 20158406,
+        'cgcd': '4116114537'
+      },
+      'invoices': [
+        {
+          'branchId': 630,
+          'invoiceId': 509172,
+          'invoiceDate': '27/04/2017',
+          'customerName': 'CRISTIANO PEREIRA ABADE',
+          'invoiceType': ' ',
+          'erpId': 369492504,
+          'address': 'CARLOS PIVA, 493 CASA',
+          'neighborhood': 'JD TRES MARIAS',
+          'zipcode': 8331,
+          'zipcodeComplement': 180,
+          'zipcodeComplete': '8331-180',
+          'city': 'SAO PAULO',
+          'state': 'SP',
+          'phoneAreaCode': 11,
+          'phoneNumber': 20158406,
+          'cgcd': '4116114537'
+        }
+      ]
+    }
+  ],
+  'slipProducts': [
+    {
+      'product': '088749100',
+      'image': '088749100.jpg',
+      'name': 'Sanduicheira/Mini Grill Cadence Easy Meal Color',
+      'idSeller': 'magazineluiza',
+      'nameSeller': 'Magazine Luiza',
+      'skuMarketPlace': null,
+      'quantity': '01',
+      'totalItem': '63.42',
+      'scheduledDeliveryDate': null,
+      'scheduledPeriod': null,
+      'expectedDateMessage': null
+    },
+    {
+      'product': '023001100',
+      'image': '023001100.jpg',
+      'name': 'Kit 3 em 1 Britânia com Liquidificador Batedeira',
+      'idSeller': 'magazineluiza',
+      'nameSeller': 'Magazine Luiza',
+      'skuMarketPlace': null,
+      'quantity': '02',
+      'totalItem': '423.12',
+      'scheduledDeliveryDate': null,
+      'scheduledPeriod': null,
+      'expectedDateMessage': null
+    }
+  ],
+  'deliveryAddress': {
+    'id': 31584028,
+    'orderId': 32736329,
+    'address': 'CARLOS PIVA',
+    'district': 'Jardim três Marias',
+    'city': 'SAO PAULO',
+    'state': 'SP',
+    'zipCode': '08331180 ',
+    'number': '493',
+    'complement': 'Casa',
+    'name': 'Cristiano Pereira Abade'
+  },
+  'cardCredit': {
+    'id': 19591936,
+    'orderId': 32736329,
+    'administration': 'Visa',
+    'number': '****6597'
+  },
+  'installmentPayment': 6,
+  'installmentAmount': '82.50',
+  'installmentOtherAmount': '82.50',
+  'corporate': 0,
+  'rate': false,
+  'tax': 'sem Juros',
+  'datePaymentBillet': 'Sexta-feira, 28 de Abril',
+  'emailTemplateRef': '86-8806'
+};
+
+const finalOrderJSON = deepmerge.all([newOrderJSON, invoiceJSON, pickUpOrderJSON]);
 
 firstView(finalOrderJSON);
-debugView(deepmerge);
+debugView(deepmerge.all);
